f0847a1b099d861c14a11e6e6f3acc180dc40e6e
emori <gringin31@gmail.com>
2023-10-28 01:42:14 +0900

    8 commit

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 python3 -m pip install --upgrade pip
+pip install django-widgets-improved
+pip install django-stdimage
 pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
